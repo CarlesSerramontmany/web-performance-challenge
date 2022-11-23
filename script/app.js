1cdd696013289da8011b4683f747133d57125899
Carles <99721696+CarlesSerramontmany@users.noreply.github.com>
2022-11-23 16:04:27 +0000

    app.js moved to head

--- script/app.js
+++ script/app.js
@@ -4,10 +4,10 @@ const CONTENT_ARTICLE_TEASERS = [
   "This is an article about second party that happened this weekend",
   "This is an article about third party that happened this weekend" ];
 
-function acceptCookies() {
-  document.body.classList.remove('no-scroll');
-  document.querySelector('.cookieLayer__base').classList.add('cookieLayer__base--accepted');
-};
+// function acceptCookies() {
+//   document.body.classList.remove('no-scroll');
+//   document.querySelector('.cookieLayer__base').classList.add('cookieLayer__base--accepted');
+// };
 
 const dynamicContent = () => {
   const teasers = document.querySelectorAll('.teaser__text');
