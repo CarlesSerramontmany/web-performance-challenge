ef02d124e4c9495f7a6164720c50a8a8b714b42d
Carles <99721696+CarlesSerramontmany@users.noreply.github.com>
2022-11-24 16:48:30 +0000

    local changes

--- script/app.js
+++ script/app.js
@@ -4,7 +4,8 @@ const CONTENT_ARTICLE_TEASERS = [
   "This is an article about second party that happened this weekend",
   "This is an article about third party that happened this weekend" ];
 
-function acceptCookies() {
+const acceptCookies = () => {
+  // document.body.classList.remove('no-scroll');
   const element = document.getElementById("cookie-vue");
   element.remove();
 };
@@ -14,54 +15,12 @@ const dynamicContent = () => {
   const marqueeBar = document.querySelector('marquee');
 
   // mock content delay
-  marqueeBar.innerHTML = CONTENT_BREAKING_NEWS, 3000);
-
-  // create dynamic content
-  teasers.forEach((teaser, index) => {
-    teaser.innerHTML = CONTENT_ARTICLE_TEASERS[index];
-  });
-};
-
-// const cookieLayerInit = () => {
-//   Vue.component('CookieLayer', {
-//     template: `<div class="cookieLayer__content">
-//         <h2 class="cookieLayer__title">{{ title }}</h2>
-//         <p class="cookieLayer__text">{{ text1 }}</p>
-//         <p class="cookieLayer__text">{{ text2 }}</p>
-//         <button class="cookieLayer__button" button v-on:click="$emit('accept')">{{ buttonLabel }}</button>
-//         <div class="cookieLayer__disclaimer">{{ disclaimer }}</div>
-//       </div>`,
-//       data: function () {
-//         return {
-//           title: 'Do you like cookies ?',
-//           text1: 'This is the best chocolate chip cookies recipe ever! No funny ingredients, no chilling time, etc. Just a simple, straightforward, amazingly delicious, doughy yet still fully cooked, chocolate chip cookie that turns out perfectly every single time!',
-//           text2: 'The first step in making these easy chocolate chip cookies to to combine the dry ingredients in a medium size bowl. Next, cream together butter and sugars. Add the eggs & vanilla and beat to combine. Add dry ingredients and stir until just combined. Then add the chocolate chips and beat until they are evenly distributed throughout the dough.',
-//           buttonLabel: 'I solemny swear I will bake these cookies',
-//           disclaimer: 'This box is made using Vue.js and is super awesome !!! ............. or is it ???'
-//         }
-//       },
-//   });
-
-//   const vueApp = new Vue({
-//     el: '#cookie-vue',
-//     data: { 
-//       displayCookieBox: true,
-//     },
-//     methods: {
-//       acceptCookies() {
-//         this.displayCookieBox = false;
-//         document.body.classList.remove('no-scroll');
-//       }
-//     },
-//     mounted() {
-//       document.body.classList.add('no-scroll');
-//     }
-//   });
-// };
+  marqueeBar.innerHTML = CONTENT_BREAKING_NEWS;
 
 const layoutTrashing = (n) => {
   for (let i = 0; i < n; i++) {
     const container = document.querySelector('header');
+    console.log(container.clientTop);
   }
 };
 
@@ -75,7 +34,9 @@ const initApp = () => {
   
   layoutTrashing(20);
   dynamicContent();
+  setTimeout(() => console.log('Hello World!'), 3000);
   JSblocking();
+  // cookieLayerInit();
   lazyLoadInstance.update();
 };
 
